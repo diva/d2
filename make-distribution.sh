3be626bd1f1fb575063a41d711d1c1210e7260a4
Diva Canto <diva@metaverseink.com>
2013-04-22 08:41:44 -0700

    D2 release r22458

--- make-distribution.sh
+++ make-distribution.sh
@@ -11,7 +11,7 @@ wd=`pwd`
 #tag=`C:/Program\ Files/Git/bin/git show-ref --tags | tail -1`
 #tag=`"C:/Program Files (86)/Git/bin/git" show-ref --tags | tail -1`
 #tag=`echo ${tag:53}`
-tag=22043
+tag=22458
 distdir=diva-r$tag
 
 # Create language satellite assemblies for localization
